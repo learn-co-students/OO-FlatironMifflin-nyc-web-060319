0bd5add4f025bcc4171589f7d09fd6afa70287ed
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-06-10 21:25:12 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,20 @@
 class Manager
+  
+    attr_reader :name, :age, :department
+    @@all = []
+    def initialize(name,age, department)
+        @name = name
+        @age = age
+        @department = department
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+       departments =  all.map {|manager| manager.department}
+       departments.uniq
+    end
 end
